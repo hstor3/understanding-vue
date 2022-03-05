ac06a7104600eb9e879f95604485628c3740cb7e
Heather <heather@3iautoshot.com>
2022-03-05 17:08:22 -0500

    kanye api structured

---
+++ src/services/kanyeAPI.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  {{ quote.quote }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'kanyeAPI',
+  data() {
+    return {
+      quote: []
+    }
+  },
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch("https://api.kanye.rest");
+        this.quote = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  created() {
+    this.getData()
+  }
+};
+</script>
