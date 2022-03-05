c220539fdcc5ba642a4e31d7181459f73f805c01
Heather <heather@3iautoshot.com>
2022-03-05 17:09:10 -0500

    kanye and weather apis imported to homepage

---
+++ src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>
+        <kanyeAPI />
+        <weatherAPI />
+    </h2>
+</template>
+
+<script>
+    import kanyeAPI from '../services/kanyeAPI.vue';
+    import weatherAPI from '../services/weatherAPI.vue';
+
+    export default {
+        name: 'HomePage',
+            components: {
+                kanyeAPI,
+                weatherAPI
+            }
+    }
+</script>
