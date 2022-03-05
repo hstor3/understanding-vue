147113d8a2a79e2ef02e9a5c44f2754b3ad2f9ea
Heather <heather@3iautoshot.com>
2022-03-05 17:08:51 -0500

    initial weather api structured

---
+++ src/services/weatherAPI.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  {{ weather.weather }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'weatherAPI',
+  data() {
+    return {
+      weather: []
+    }
+  },
+  methods: {
+    async getData() {
+      try {
+        let name = 'Holland';
+        let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + name + `&appid=${process.env.VUE_APP_APIKEY}&units=imperial`);
+        this.weather = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  created() {
+    this.getData()
+  }
+};
+</script>
