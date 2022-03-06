54a1dc22bbcbbaf32666720474c1d50cb26413c3
Heather <heather@3iautoshot.com>
2022-03-06 08:30:24 -0500

    about added to homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,33 @@
 <template>
+  <chicagoWeather />
   <kanyeComponent />
-  <seattleWeather />
+  <div>
+    <button class='btn border' :class="{toggle: !toggle}" v-on:click='toggleAbout'>{{this.toggle === false ? 'SHOW' : 'HIDE'}}</button>
+  </div>
+  <aboutSection />
 </template>
 
 <script>
   import kanyeComponent from '../components/kanyeQuote.vue';
-  import seattleWeather from '../components/seattleWeather.vue';
+  import chicagoWeather from '../components/chicagoWeather.vue';
+  import aboutSection from '../components/about.vue';
 
   export default {
     name: 'HomePage',
     components: {
       kanyeComponent,
-      seattleWeather
+      chicagoWeather,
+      aboutSection
+    },
+    data() {
+      return {
+        toggle: false
+      }
+    },
+    methods: {
+      toggleAbout: function toggleAbout() {
+        this.toggle ? true : false
+      }
     }
   }
 </script>
