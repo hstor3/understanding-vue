cbf5eb6ec740ed89335b84070a2d3f09087b2aca
Heather <heather@3iautoshot.com>
2022-03-05 19:25:22 -0500

    api imported to component

--- src/components/kanyeQuote.vue
+++ src/components/kanyeQuote.vue
@@ -1,5 +1,14 @@
 <template>
+    <kanyeAPI />
 </template>
 
 <script>
+    import kanyeAPI from '../services/kanyeAPI.vue';
+
+    export default {
+        name: 'kanyeComponent',
+        components: {
+            kanyeAPI
+        }
+    }
 </script>
--- src/components/seattleWeather.vue
+++ src/components/seattleWeather.vue
@@ -1,5 +1,14 @@
 <template>
+  <weatherAPI />
 </template>
 
 <script>
+  import weatherAPI from '../services/weatherAPI.vue';
+
+  export default {
+    name: 'seattleWeather',
+    components: {
+      weatherAPI
+    }
+  }
 </script>
