477e6e0b61298fd38784f44a9a0f1e1a4262f5bd
Heather <heather@3iautoshot.com>
2022-03-06 08:27:57 -0500

    about page

---
+++ src/components/about.vue
@@ -0,0 +1,18 @@
+<template>
+<div class='bg-secondary mt-3 p-3 rounded text-white-50'>
+    <p>
+      This is a basic Vue App that contains internal CSS (ew, sorry), bootstrap, Vue, JavaScript, and HTML.
+      This site uses the 
+      <a class='text-decoration-none text-black-50' href="https://openweathermap.org/current" target="_blank">Open Weather API</a>
+      , the 
+      <a class='text-decoration-none text-black-50' href="https://kanye.rest/?ref=apitemple" target="_blank">Kanye Rest API</a>
+      , and was created to help me quickly learn the VueJs basics.
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'aboutSection'
+  }
+</script>
