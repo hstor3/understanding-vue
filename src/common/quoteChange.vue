2c6df984e14cd76374b80635baab164176bac064
Heather <heather@3iautoshot.com>
2022-03-06 08:26:40 -0500

    new quote common created

---
+++ src/common/quoteChange.vue
@@ -0,0 +1,14 @@
+<template>
+  <button v-on:click='refresh' class='my-5 btn btn-warning'>New Quote</button>
+</template>
+
+<script>
+  export default {
+    name: 'refreshQuote',
+    methods: {
+      refresh: function () {
+        location.reload()
+      }
+    }
+  }
+</script>
