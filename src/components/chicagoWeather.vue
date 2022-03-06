507fd63090bbe6f2898034711eb28ee2a5f16995
Heather <heather@3iautoshot.com>
2022-03-06 08:28:53 -0500

    weather component

---
+++ src/components/chicagoWeather.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='h-25 mb-3 bg-warning text-white rounded'>
+    <h3 class='weather-container py-3'>
+      <weatherAPI />
+    </h3>
+  </div>
+</template>
+
+<script>
+  import weatherAPI from '../services/weatherAPI.vue';
+
+  export default {
+    name: 'chicagoWeather',
+    components: {
+      weatherAPI
+    }
+  }
+</script>
+
+<style>
+  .weather-container {
+    transform: translate(0%, 15%);
+  }
+</style>
