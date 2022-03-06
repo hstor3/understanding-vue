ad258d4a58f61bc810b4f7f82a6fa2d4865d989f
Heather <heather@3iautoshot.com>
2022-03-06 08:25:49 -0500

    app styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HomePage />
 </template>
 
@@ -21,6 +20,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh!important;
+  margin: 3%;
+  position: fixed;
+  width: -webkit-fill-available;
 }
 </style>
