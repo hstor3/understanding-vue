ac84aa4d183110a82797201ff5e7b080074de3fd
Heather <heather@3iautoshot.com>
2022-03-06 08:29:15 -0500

    import new quote button

--- src/components/kanyeQuote.vue
+++ src/components/kanyeQuote.vue
@@ -1,14 +1,28 @@
 <template>
-    <kanyeAPI />
+  <div class='h-50 bg-dark text-white rounded'>
+    <h3 class='quote-container h-100'>
+      <div class='text-danger'>Quotes by Kanye:</div>
+      <kanyeAPI />
+      <refreshQuote />
+    </h3>
+  </div>
 </template>
 
 <script>
-    import kanyeAPI from '../services/kanyeAPI.vue';
+  import kanyeAPI from '../services/kanyeAPI.vue';
+  import refreshQuote from '../common/quoteChange.vue';
 
-    export default {
-        name: 'kanyeComponent',
-        components: {
-            kanyeAPI
-        }
+  export default {
+    name: 'kanyeComponent',
+    components: {
+      kanyeAPI,
+      refreshQuote
     }
+  }
 </script>
+
+<style>
+  .quote-container {
+    transform: translate(0%, 20%);
+  }
+</style>
