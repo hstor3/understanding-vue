f677371afec59bbc61907d17571133f6208fbf2e
Heather <heather@3iautoshot.com>
2022-03-06 11:17:03 -0500

    link to apis

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,11 +1,15 @@
 <template>
-<div class='bg-secondary mt-3 p-3 rounded text-white-50'>
+<div class='bg-secondary mt-3 py-2 rounded text-white-50'>
     <p>
       This is a basic Vue App that contains internal CSS (ew, sorry), bootstrap, Vue, JavaScript, and HTML.
       This site uses the 
-      <a class='text-decoration-none text-black-50' href="https://openweathermap.org/current" target="_blank">Open Weather API</a>
+      <a class='text-decoration-none text-black-50' :href="openWeatherURL" target="_blank">
+        Open Weather API
+      </a>
       , the 
-      <a class='text-decoration-none text-black-50' href="https://kanye.rest/?ref=apitemple" target="_blank">Kanye Rest API</a>
+      <a class='text-decoration-none text-black-50' :href="kanyeRestURL" target='_blank'>
+        Kanye Rest API
+      </a>
       , and was created to help me quickly learn the VueJs basics.
     </p>
   </div>
@@ -13,6 +17,12 @@
 
 <script>
   export default {
-    name: 'aboutSection'
+    name: 'aboutSection',
+    data() {
+      return {
+        kanyeRestURL: 'https://kanye.rest/?ref=apitemple',
+        openWeatherURL: 'https://openweathermap.org/current'
+      }
+    }
   }
 </script>
